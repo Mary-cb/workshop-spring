db1959629e726f0147644e198911a18260b3a049
Clara <clarabsantos02@gmail.com>
2024-05-07 21:44:48 -0300

    Service layer, component registration

--- demo/src/main/java/com/springprojeto/demo/repositories/UserRepository.java
+++ demo/src/main/java/com/springprojeto/demo/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.springprojeto.demo.repositories;
 
 import com.springprojeto.demo.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
